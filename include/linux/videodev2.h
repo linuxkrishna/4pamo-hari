90e9133e20fe54769cb2ac67b7aca54ca64c3ac3
Sergio Aguirre <saaguirre@ti.com>
2008-10-17 14:46:58 -0500

    V4L: Add RAW10 Pattern format MMS
    This adds MMS changes to the OMAP34xx camera driver. Including:
    - HQ capture
    
    NOTE: Credits to MMS crew for all this.
    
    Signed-off-by: Sergio Aguirre <saaguirre@ti.com>

--- include/linux/videodev2.h
+++ include/linux/videodev2.h
@@ -319,6 +319,11 @@ struct v4l2_pix_format {
 #define V4L2_PIX_FMT_SGRBG10DPCM8 v4l2_fourcc('B', 'D', '1', '0') /* 10bit raw bayer DPCM compressed to 8 bits */
 #define V4L2_PIX_FMT_SBGGR16 v4l2_fourcc('B', 'Y', 'R', '2') /* 16  BGBG.. GRGR.. */
 
+/* Added by MMS */
+#define V4L2_PIX_FMT_PATT     v4l2_fourcc('P', 'A', 'T', 'T') /* 10-bit raw
+							       * pattern
+							       */
+
 /* compressed formats */
 #define V4L2_PIX_FMT_MJPEG    v4l2_fourcc('M', 'J', 'P', 'G') /* Motion-JPEG   */
 #define V4L2_PIX_FMT_JPEG     v4l2_fourcc('J', 'P', 'E', 'G') /* JFIF JPEG     */
